0b4b3c2fd97a8e4ab3b91bef8fd3f3b1085a027f
Jody Bruchon <jody@jodybruchon.com>
2020-07-10 23:37:51 -0400

    First batch of C snippets, plus a spiffy README explaining things

---
+++ jody_sort.h
@@ -0,0 +1,18 @@
+/* Jody Bruchon's sorting code library
+ * See jody_sort.c for license information */
+
+#ifndef JODY_SORT_H
+#define JODY_SORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int numeric_sort(const char * restrict c1,
+                const char * restrict c2, int sort_direction);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* JODY_SORT_H */
---
+++ jody_win_unicode.h
@@ -0,0 +1,27 @@
+/* Jody Bruchon's Windows Unicode helper routines
+ * See jody_win_unicode.c for license information */
+
+#ifndef JODY_WIN_UNICODE_H
+#define JODY_WIN_UNICODE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "jdupes.h"
+#include <stdio.h>
+
+extern int fwprint(FILE * const restrict stream, const char * const restrict str, const int cr);
+
+#ifdef UNICODE
+extern void slash_convert(char *path);
+extern void widearg_to_argv(int argc, wchar_t **wargv, char **argv);
+#else
+ #define slash_convert(a)
+#endif /* UNICODE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* JODY_WIN_UNICODE_H */
